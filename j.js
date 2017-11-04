7c96bfac51e83056892aa7296dc220320dfa3716
Van Pho Nguyen <vanpho02@gmail.com>
2017-11-04 13:38:20 +0700

    file

--- j.js
+++ j.js
@@ -4,23 +4,42 @@
 2. In ra cac so le trong khoang tu 0 ... 100
 3. In ra cac so chia 3 du 1 trong khoang tu 0 ... 100
 4. In ra cac so chinh phuong trong khoang tu 0 ... 100
-
+5. In ra cac so thoa man dieu kien gi do trong khoang tu 0 - 100
 */
 
 function inSoChan() {
-    for(let i = 0; i <= 100; i += 2) console.log(i);
+    for(let i = 0; i <= 100; i += 1) {
+        const dk = i % 2 === 0;
+        if(dk) console.log(i);
+    }
 }
 
 function inSoLe() {
-    for(let i = 1; i <= 100; i += 2) console.log(i);
+    for(let i = 0; i <= 100; i += 1) {
+        const dk = i % 2 === 1;
+        if(dk) console.log(i);
+    }
 }
 
-function inSoChia3Du1() {
-    for(let i = 1; i <= 100; i += 3) console.log(i);
+function inSo(soDu) {
+    for(let i = 0; i <= 100; i += 1) {
+        const dk = i % 2 === soDu;
+        if(dk) console.log(i);
+    }
 }
 
-function inSoChinhPhuong() {
-    for(let i = 1; i <= 10; i += 1) console.log(i * i);
-}
+inSo(0);
 
-inSoChinhPhuong();
+// function inSoChia3Du1() {
+//     for(let i = 0; i <= 100; i += 1) {
+//         const dk = i % 3 === 1;
+//         if(dk) console.log(i);
+//     }
+// }
+
+// function inSoChinhPhuong() {
+//     for(let i = 0; i <= 100; i += 1) {
+//         const dk = Math.sqrt(i) % 1 === 0;
+//         if(dk) console.log(i);
+//     }
+// }
