9e17207e16b51b3f1967539c10191bc6d8c6da3b
Van Pho Nguyen <vanpho02@gmail.com>
2017-11-04 15:00:20 +0700

    perimeter

--- m.js
+++ m.js
@@ -13,8 +13,33 @@ class Line {
         this.pointA = pointA;
         this.pointB = pointB;
     }
+
+    getLength() {
+        const dx = this.pointA.x - this.pointB.x;
+        const dy = this.pointA.y - this.pointB.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+}
+
+class Triangle {
+    constructor(pointA, pointB, pointC) {
+        this.pointA = pointA;
+        this.pointB = pointB;
+        this.pointC = pointC;
+    }
+
+    getPerimeter() {
+        const line1 = new Line(this.pointA, this.pointB);
+        const line2 = new Line(this.pointA, this.pointC);
+        const line3 = new Line(this.pointB, this.pointC);
+        return line1.getLength() + line2.getLength() + line3.getLength();
+    }
 }
 
 const p1 = new Point(0, 0);
 const p2 = new Point(1, 0);
+const p3 = new Point(0, 1);
 const l1 = new Line(p1, p2);
+const t1 = new Triangle(p1, p2, p3);
+console.log(l1.getLength());
+console.log(t1.getPerimeter());
