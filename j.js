d45a1fd5de9d9701af03a3aaa22bc4fd315b2dee
Van Pho Nguyen <vanpho02@gmail.com>
2017-11-04 14:01:36 +0700

    arrow function

--- j.js
+++ j.js
@@ -21,25 +21,45 @@ function inSoLe() {
     }
 }
 
-function inSo(soDu) {
+function inSoChia3Du1() {
     for(let i = 0; i <= 100; i += 1) {
-        const dk = i % 2 === soDu;
+        const dk = i % 3 === 1;
         if(dk) console.log(i);
     }
 }
 
-inSo(0);
+function inSoChinhPhuong() {
+    for(let i = 0; i <= 100; i += 1) {
+        const dk = Math.sqrt(i) % 1 === 0;
+        if(dk) console.log(i);
+    }
+}
 
-// function inSoChia3Du1() {
+function inSo(checkNumber) {
+    for(let i = 0; i <= 100; i += 1) {
+        const dk = checkNumber(i); // kiemTraSoChinhPhuong(i);
+        if(dk) console.log(i);
+    }
+}
+
+// function kiemTraSoChan(n) {
+    //     return n % 2 === 0;
+    // }
+
+    // function kiemTraSoChinhPhuong(n) {
+    //     return Math.sqrt(n) % 1 === 0;
+// }
+
+inSo(n => n % 2 === 0);
+inSo(x => x % 2 === 1);
+inSo(x => x % 3 === 1);
+inSo(x => Math.sqrt(x) % 1 === 0);
+
+// function inSo(soChia, soDu) {
 //     for(let i = 0; i <= 100; i += 1) {
-//         const dk = i % 3 === 1;
+//         const dk = i % soChia === soDu;
 //         if(dk) console.log(i);
 //     }
 // }
 
-// function inSoChinhPhuong() {
-//     for(let i = 0; i <= 100; i += 1) {
-//         const dk = Math.sqrt(i) % 1 === 0;
-//         if(dk) console.log(i);
-//     }
-// }
+// inSo(2, 1);
